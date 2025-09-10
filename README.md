7b7d549e0a024ad3702a7f181a020daf86a5ff5e
pramod ade <prmdextracthere@gmail.com>
2025-09-10 20:47:38 +0530

    complete

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 #### Create a LapTimer using useEffect hook and other hooks of React to get output as shown below
 
 <img src='https://storage.googleapis.com/acciojob-open-file-collections/lap-timer_AdobeExpress.gif' height='250px' alt='output'/>
+
+
+## lets start making the project.
+
+--> first thing to know is format of time
+
+100 centisecond means 1 second.
+so using this.
+
+1 minute = centisecond / 6000; 
+ 60 second 1 minute 
+ 1 second means 100 centisecond.
+ 100 * 60= 6000 centisecond means 1 minute.
+so when we want minute out of centisecond. we do (centisecond/6000);
+
+for ex: 7543 centisecond means (7543/6000)  1 minute. 
+
+## Minute
+centisecond / 6000
+
+## second
+(centisecond % 6000) we separate the minute from it so remaining / 100 is second. because 1 sec is 100 centisecond.
+
+(centisecond % 6000) / 1000;
+
+## centisecond 
+centisecond % 100 
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,74 @@
 
-import React from "react";
+import React, { useEffect, useState,useRef } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [time,setTime] = useState(0); // initally time is zero
+  const [laps,setLaps] = useState([]); // initally laps is empty array later we fill it.
+  const timeRef = useRef(null);
+
+  useEffect(()=>{
+    return () => clearInterval(timeRef.current);
+  },[]);
+
+
+  const startTimer=()=>{
+    if(!timeRef.current){
+      timeRef.current = setInterval(()=>{
+        setTime((prev)=>prev+1);
+      },10);
+    }
+  };
+
+  /* so above block is for calculation time. befor start timer we chake the there is already calculation time or not. if no then we set inteval 10 ms and add. because we counting in centisecond
+  1 centisecond = 10ms; so this way the time is calculation
+  */
+
+  const stopTimer = () =>{
+    clearInterval(timeRef.current);
+    timeRef.current = null;
+  }
+
+  const resetTimer = () =>{
+    stopTimer();
+    setTime(0);
+    setLaps([]);
+
+  }
+
+  const recordLap = () =>{
+    setLaps((prev)=>[...prev,time]);
+    console.log(laps);
+  }
+
+  const formatTime = (centisecond) =>{
+    const minute = Math.floor(centisecond /6000);
+    const second = Math.floor((centisecond % 6000)/100);
+    const centis = centisecond % 100;
+
+    const pad = (num) => String(num).padStart(2,0);
+
+    return `${pad(minute)}:${pad(second)}:${pad(centis)}`
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+     <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{formatTime(time)}</h1>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={recordLap}>Lap</button>
+      <button onClick={resetTimer}>Reset</button>
+
+      <h2>Laps</h2>
+      <ul style={{listStyleType: "none"}}>
+        {laps.map((lapTime,index)=>{
+          return( <li key={index+1}>{formatTime(lapTime)}</li>)
+         
+        })}
+        
+      </ul>
     </div>
   )
 }
 
-export default App
+export default App;
